00c20bcb922a093c7cf6e0094b3420618437d189
Bob Stalmach <bob.stalmach@gmail.com>
2015-03-25 01:11:51 +0100

    Working version
    Basic processing is not split between processor and filter. Added meaningful tests.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 'use strict';
 
-hexo.extend.generator.register('multiauthor', require('./lib/multiauthor'));
+// Model
+hexo.database.model('Author', require('./lib/model'));
+
+//Processor - to load and parse authors
+hexo.extend.processor.register('_authors/*.yml', require('./lib/processor'));
+
+//Filter - to add author data to Post
+hexo.extend.filter.register('before_post_render', require('./lib/filter.js'));
