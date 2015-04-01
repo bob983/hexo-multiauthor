bcc7ad7a00ad9b7f43df16f65d359d05cfe5a406
Bob Stalmach <bob.stalmach@gmail.com>
2015-04-01 09:18:39 +0200

    Working, but slow

--- test/generatorSpec.js
+++ test/generatorSpec.js
@@ -3,25 +3,32 @@
 var Hexo = require('hexo');
 var assert = require("assert");
 var hexo = new Hexo(__dirname, {silent: true});
-var Author = hexo.model('author');
+var Author = hexo.model('Author');
+var Post = hexo.model('Post');
+var Promise = require('bluebird');
+
 
 describe('Generator', function () {
     var generator = require('../lib/generator').bind(hexo);
 
+    before(function () {
+        var authorPromise = Author.insert(
+            {id: 'ABC', name: 'AaBbCc', source: 'blah'}
+        );
+        var postPromise = Post.insert([
+                {id: 'ABC1', name: 'AaBbCc', source: 'blah', slug: 'blah', authorId: 'ABC'},
+                {id: 'ABC2', name: 'AaBbCc', source: 'blah', slug: 'blah', authorId: 'ABC'}
+            ]
+        );
+        return Promise.all([authorPromise, postPromise]).then(function () {
+
+        }).done();
+    });
 
     it('should group posts by author', function () {
-        var locals = {
-            posts: [{
-                author: {id: 'ABC'}
-            }, {
-                author: {id: 'ABC'}
-            },
-                {
-                    author: {id: 'XYZ'}
-                }]
-        };
-        var content = generator(locals);
-        console.info('content', content);
+        var content = generator();
+        assert.equal(content.length, 1);
     });
+
 
 });
